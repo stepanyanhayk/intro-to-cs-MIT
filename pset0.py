e16ef681e8e228a122c1dc6c5f9a4ac0f4d19ddc
stepanyanhayk <54330853+stepanyanhayk@users.noreply.github.com>
2020-05-23 10:12:03 +0400

    Add files via upload

---
+++ pset0.py
@@ -0,0 +1,11 @@
+"""
+Created on May 19, 2020
+
+@author: Hayk Stepanyan
+"""
+import math
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print(x ** y)
+print(math.log2(x))
